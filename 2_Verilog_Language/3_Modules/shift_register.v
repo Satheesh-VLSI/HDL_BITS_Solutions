53eb60e50733e03578df452fd17af37b8b3abb55
Satheesh <satheeshnarayan05@gmail.com>
2026-03-20 09:42:03 +0530

    Create shift_register.v

---
+++ 2_Verilog_Language/3_Modules/shift_register.v
@@ -0,0 +1,13 @@
+/*You are given a module my_dff with two inputs and one output (that implements a D flip-flop). Instantiate three of them, then chain them together to make a shift register of length 3. The clk port needs to be connected to all instances.
+
+The module provided to you is: module my_dff ( input clk, input d, output q );*/
+
+module top_module ( input clk, input d, output q );
+    wire q1,q2,q3;
+    my_dff d1(clk,d,q1);
+    my_dff d2(clk,q1,q2);
+    my_dff d3(clk,q2,q3);
+    assign q=q3;
+    
+    
+endmodule
