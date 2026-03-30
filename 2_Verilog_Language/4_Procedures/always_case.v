e4a630c2c08991f530238477e62166fa3e1c6408
Satheesh <satheeshnarayan05@gmail.com>
2026-03-30 10:36:00 +0530

    Update always_case.v

--- 2_Verilog_Language/4_Procedures/always_case.v
+++ 2_Verilog_Language/4_Procedures/always_case.v
@@ -1,3 +1,5 @@
+/*Case statements are more convenient than if statements if there are a large number of cases. So, in this exercise, create a 6-to-1 multiplexer. 
+When sel is between 0 and 5, choose the corresponding data input. Otherwise, output 0. The data inputs and outputs are all 4 bits wide.*/
 // synthesis verilog_input_version verilog_2001
 module top_module ( 
     input [2:0] sel, 
