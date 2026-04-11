d8f0717dc25de419c92e7d1632de8d2fc64848ed
Satheesh <satheeshnarayan05@gmail.com>
2026-04-11 18:02:12 +0530

    Create adder_100_bit.v

---
+++ 2_Verilog_Language/5_More_features/adder_100_bit.v
@@ -0,0 +1,21 @@
+/*Create a 100-bit binary ripple-carry adder by instantiating 100 full adders. 
+The adder adds two 100-bit numbers and a carry-in to produce a 100-bit sum and carry out.
+To encourage you to actually instantiate full adders, also output the carry-out from each full adder in the ripple-carry adder. 
+cout[99] is the final carry-out from the last full adder, and is the carry-out you usually see.*/
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    genvar i;
+    generate
+        add1 a1(a[0],b[0],cin,sum[0],cout[0]);
+        for(i=1;i<100;i=i+1)begin:adder
+            add1 ai(a[i],b[i],cout[i-1],sum[i],cout[i]);
+        end
+    endgenerate
+
+endmodule
+module  add1 (input a,b,cin,output sum,cout);
+    assign {cout,sum}=a+b+cin;
+endmodule
