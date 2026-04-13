82ef8169e8fc91c7909fab0704fba7b64e191f8c
Satheesh <satheeshnarayan05@gmail.com>
2026-04-13 10:00:52 +0530

    Create 2_GND.v

---
+++ 3_Circuits/1_Combinational/1_Basic_gates/2_GND.v
@@ -0,0 +1,12 @@
+/*Implement the following circuit:
+out________
+           |
+         _____
+          ___
+           _
+           -
+*/
+module top_module (
+    output out);
+assign out=1'b0;
+endmodule
