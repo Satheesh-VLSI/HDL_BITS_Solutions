22636afe3ad53dd91bdbe443e109728d9ce0cbd7
Satheesh <satheeshnarayan05@gmail.com>
2026-04-13 10:20:47 +0530

    Create 5_two gates.v

---
+++ 3_Circuits/1_Combinational/1_Basic_gates/5_two gates.v
@@ -0,0 +1,15 @@
+/*
+  in1 ───┬─── XOR ───┐
+         │           │
+  in2 ───┼───•───────┼── XOR ─── out
+         │           │
+  in3 ───┴───────────┘
+*/
+
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+    assign out=in3 ^ ~(in1^in2);
+endmodule
