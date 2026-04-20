a476cdb6285e33e8bd68677115fecf9377890ecf
Satheesh <satheeshnarayan05@gmail.com>
2026-04-20 21:49:44 +0530

    Create 3_LFSR_3-bit.v

---
+++ 3_Circuits/2_Sequential/3_Shift_Registers/3_LFSR_3-bit.v
@@ -0,0 +1,14 @@
+module top_module (
+	input [2:0] SW,      // R
+	input [1:0] KEY,     // L and clk
+	output [2:0] LEDR);  // Q
+    
+    always@(posedge KEY[0])begin
+         LEDR <= {
+            (KEY[1]?SW[2]:(LEDR[1]^LEDR[2])), // Q2
+            (KEY[1]?SW[1]:LEDR[0]),             // Q1
+            (KEY[1]?SW[0]:LEDR[2])              // Q0
+        };  end
+
+
+endmodule
