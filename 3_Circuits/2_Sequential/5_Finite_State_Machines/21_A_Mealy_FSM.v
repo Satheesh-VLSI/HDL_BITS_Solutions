fec4cd67f03874ea82a86d3567878a8664d23819
Satheesh <satheeshnarayan05@gmail.com>
2026-04-23 23:38:12 +0530

    Create 21_A_Mealy_FSM.v

---
+++ 3_Circuits/2_Sequential/5_Finite_State_Machines/21_A_Mealy_FSM.v
@@ -0,0 +1,29 @@
+module top_module (
+    input clk,
+    input aresetn,    // Asynchronous active-low reset
+    input x,
+    output z );
+    localparam S0=0,S1=1,S2=2;
+    reg [1:0] state,next;
+
+
+    always@(posedge clk or negedge aresetn) begin
+        if (!aresetn)
+            state <= S0;
+        else
+            state <= next;
+    end
+
+    // Next state logic
+    always@(*) begin
+        case (state)
+            S0:next=x?S1:S0;
+            S1:next=x?S1:S2;
+            S2:next=x?S1:S0;
+            default: next=S0;
+        endcase
+    end
+
+   
+    assign z = (state == S2 && x);
+endmodule
